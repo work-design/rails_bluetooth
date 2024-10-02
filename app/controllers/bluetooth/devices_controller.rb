cb25574f2e0b4655fbd6c362af6af8668d705408
qinmingyuan <mingyuan0715@foxmail.com>
2024-10-02 17:52:22 +0800

    devices

--- app/controllers/bluetooth/devices_controller.rb
+++ app/controllers/bluetooth/devices_controller.rb
@@ -3,6 +3,8 @@ module Bluetooth
 
     def index
       @devices = Device.default_where(default_params).page(params[:page])
+
+      render json: { devices: @devices.pluck(:name) }
     end
 
   end
